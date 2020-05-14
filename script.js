9188856054a00684d36f5d41cce0e75957795c56
Nolan Kovacik <noltron000@gmail.com>
2020-05-14 16:48:39 -0400

    just testing...

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// override mime type
-express.static.mime.define({'application/javascript': ['js']})
-
 import Maze from './node_modules/maze-algorithms/lib/index.js'
+
+console.log('gotcha')
+
 // declare elements, which will be kept track of.
 const el = {}
 el.maze = document.getElementById('maze')
 el.data = el.maze.getElementsByTagName('data')
 
 console.log(Maze([2,3]))
+
+import test from './testing.js'
+test()
